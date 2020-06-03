c54fb609b198f10bbab7ed234072b45f833c6dfc
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-03 20:34:08 +0530

    Hello world

---
+++ Hello World.kt
@@ -0,0 +1,14 @@
+// Empty Program
+
+// Single line comment
+/* Multi ,line comment */
+
+fun main() {
+    // program code comes here
+    print("Hello World 2")
+    println("Hello World 1")
+    println("Hello World 3")
+    print("Hello World 4")
+
+
+}
