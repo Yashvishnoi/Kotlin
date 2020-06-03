ba18ece636cd10ae963616d7bbf272ae5cfe99eb
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-03 23:41:03 +0530

    Operators

---
+++ Operator.kt
@@ -0,0 +1,34 @@
+fun main() {
+    val number1 = 12.5
+    val number2 = 3.5
+
+    var result : Double
+    var results : Boolean
+
+    result = number1+ number2
+    println(result)
+
+    result = number1 - number2
+    println(result)
+    result = number1 * number2
+    println(result)
+    result = number1 / number2
+    println(result)
+    result = number1 % number2
+    println(result)
+
+    results = number1 == number2
+    println(results)
+
+    results = number1!=number2
+    println(results)
+    results = number1 > number2
+    println(results)
+    results = number1 < number2
+    println(results)
+    results = number1 >= number2
+    println(results)
+    results = number1 <= number2
+    println(results)
+
+}
