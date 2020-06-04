1cecbf76582c1c05eec4925d008deb9720decf2b
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-04 22:45:59 +0530

    User defined functions

---
+++ Functions.kt
@@ -0,0 +1,14 @@
+fun main() {
+    iAmNewfunction()
+    println(timesTwo(23))
+    println(timesThree (12))
+}
+
+fun iAmNewfunction () : Unit {
+    println("I am a new Pubg Bot")
+}
+
+fun timesTwo (x:Int) : Int {
+    return x * 2
+}
+fun timesThree (x:Int) =x * 3
