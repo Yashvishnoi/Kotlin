bcaafade8fef64b7ed506da5798a0a09377193b3
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-05 21:59:35 +0530

    Greater of 2 number

---
+++ Conditionals/GreaterNumber.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val a = 410
+    val b = 210
+
+    if(a>b) {
+        println("A is greater than B")
+    }
+        else {
+            println("B is greater than A")
+        }
+
+}
