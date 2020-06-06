2dd0e9fbe2c707ceb381b670483b013acdb091a4
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-06 22:49:31 +0530

    Not Operator

---
+++ Logical Operator/NOT Operator.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a= 4
+    if(!(a>=0 && a<=5)) {
+        println("The value of integer is between 0-5 and the value is ")
+
+    }
+    else {
+        println("The value of integer is not between 0-5 ")
+
+    }
+}
