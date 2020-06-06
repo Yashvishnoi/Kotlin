388e9fd4763c3284cf66f52cd6b82b6fa45c9666
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-06 22:46:10 +0530

    Logical AND operators

---
+++ Logical Operator/And Operator.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a= 4
+    if(a>=0 && a<=5) {
+        println("The value of integer is between 0-5 and the value is ")
+        print(a)
+    }
+    else {
+        println("The value of integer is not between 0-5 ")
+        print(a)
+    }
+}
